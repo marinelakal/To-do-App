85255c5c211527276e35265c98a18902d327e14e
MARINELA KALTHI <mkalthi@draxis.gr>
2024-07-23 12:49:02 +0300

    Add a category input with submit and clear buttons

--- src/components/Categories/TheCategory.vue
+++ src/components/Categories/TheCategory.vue
@@ -1,5 +1,34 @@
 <template>
-    <section>
-        <h1>Categories page in process...</h1>
-    </section>
+
+    <v-sheet class="mx-auto" width="300">
+        <v-form fast-fail @submit.prevent>
+            <p>Add a category</p>
+            <v-text-field
+            label="Name"
+            ></v-text-field>
+
+            <div class="button-container">
+                <v-btn
+                class="me-4"
+                type="submit"
+                >
+                submit
+                </v-btn>
+
+                <v-btn>
+                clear
+                </v-btn>
+            </div>
+
+        </v-form>
+    </v-sheet>
+
+    
+                
 </template>
+
+<style scoped>
+.v-sheet {
+    margin-top: 20px;
+}
+</style>
