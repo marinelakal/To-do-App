9f4ed1201a76ccdf6ef15f46b71f440769c42513
MARINELA KALTHI <mkalthi@draxis.gr>
2024-08-20 11:10:25 +0300

    Fix Button Alignment

--- src/components/Tasks/TheTaskForm.vue
+++ src/components/Tasks/TheTaskForm.vue
@@ -40,8 +40,8 @@
       </v-container>
 
       <div class="button-container">
-        <v-btn class="submit-btn" :class="{'disabled-button': !valid}" type="submit" :disabled="!valid">submit</v-btn>
         <v-btn class="clear-btn" @click="reset">cancel</v-btn>
+        <v-btn class="submit-btn" :class="{'disabled-button': !valid}" type="submit" :disabled="!valid">submit</v-btn>
       </div>
     </v-form>
   </v-sheet>
