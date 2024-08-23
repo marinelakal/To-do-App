ebb62e6eb071d97a276cc75c1ccfd9818e1d20ae
MARINELA KALTHI <mkalthi@draxis.gr>
2024-08-23 14:17:44 +0300

    Component for Date Input

---
+++ src/components/Base/BaseDateInput.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-date-input
+      v-model="model"
+      :label="label"
+      :min="min"
+      :variant="variant"
+    >
+    </v-date-input>
+  </template>
+  
+  <script setup>
+  
+  const model = defineModel()
+  
+  defineProps({
+    label: {
+      type: String,
+      default: '',
+    },
+    min: {
+        type: String,
+        default: '',
+    },
+    variant: {
+        type: String,
+        default: 'solo', 
+    },
+  });
+  </script>
+  
