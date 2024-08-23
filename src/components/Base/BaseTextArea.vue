1c299ee1414dd895ca1d46e9ad1339cfa2be9eef
MARINELA KALTHI <mkalthi@draxis.gr>
2024-08-23 13:31:15 +0300

    Component for TextArea

---
+++ src/components/Base/BaseTextArea.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-textarea
+      v-model="model"
+      :label="label"
+      :rules="rules"
+      :required="required"
+    >
+    </v-textarea>
+  </template>
+  
+  <script setup>
+  
+  const model = defineModel()
+  
+  defineProps({
+    label: {
+      type: String,
+      default: '',
+    },
+    rules: {
+      type: Array,
+      default: () => [],
+    },
+    required: {
+      type: Boolean,
+      default: false,
+    },
+  });
+  </script>
+  
