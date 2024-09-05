5d013e2e015643d339568230480934ff4a136376
MARINELA KALTHI <mkalthi@draxis.gr>
2024-09-05 15:39:03 +0300

    Truncate Long Names to Maintain Table Format

--- src/components/Base/BaseTableRows.vue
+++ src/components/Base/BaseTableRows.vue
@@ -2,7 +2,10 @@
     <template v-for="column in columns" :key="column.key">
       <td v-if="column.visible" :class="column.key">
         <slot :name="column.key" :item="item">
-          <span v-if="column.key === 'description'">
+          <span v-if="column.key === 'name'">
+            {{ truncatedName(item[column.key] || '') }}
+          </span>
+          <span v-else-if="column.key === 'description'">
             {{ showDescriptionContent ? truncatedDescription( item[column.key] || '') : '' }}
           </span>
           <span v-else>
@@ -26,6 +29,10 @@
       default: true,
     },
     truncatedDescription: {
+      type: Function,
+      required: true,
+    },
+    truncatedName: {
       type: Function,
       required: true,
     },
