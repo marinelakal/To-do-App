1377cb52bdf9a0139d3de339f70c93c8875b4159
MARINELA KALTHI <mkalthi@draxis.gr>
2024-09-10 11:29:54 +0300

    Implement Assignees Table and Form Functionality

--- src/components/Assignees/AssigneeAppForm.vue
+++ src/components/Assignees/AssigneeAppForm.vue
@@ -34,12 +34,20 @@
 </template>
 
 <script setup>
+import { ref , watch } from 'vue';
 import BaseForm from '../Base/BaseForm.vue';
 import BaseTextField from '@/components/Base/BaseTextField.vue';
 import { useRouter } from 'vue-router';
+import { useAssigneesStore } from '@/stores/useAssigneesStore';
 
-// Access the router instance
-const router = useRouter();
+// Reactive variables
+const valid = ref(false);
+const firstname = ref('');
+const lastname = ref('');
+const email = ref('');
+const phone = ref('');
+const form = ref(null);
+
 
 // Non-reactive variables
 const FirstNameRules = [
@@ -51,18 +59,74 @@ const LastNameRules = [
 ];
 
 const EmailRules = [
-  value => (value ? true : 'Email is required.')
+  value => !!value || 'Email is required.',
+  value => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value) || 'Please enter a valid email address.'
 ];
 
 const PhoneNumberRules = [
-  value => (value ? true : 'Phone Number is required.')
+  value => !!value || 'Phone Number is required.',
+  value => /^\+[1-9]\d{0,2}([-\s]?\d){8,12}$/.test(value) || 'Please enter a valid phone number.'
 ];
 
+
+// Access the router instance
+const router = useRouter();
+
+// Access the stores
+const assigneesStore = useAssigneesStore(); 
+
+// Watch for changes in editAssigneeIndex and update the form accordingly
+watch(
+  () => assigneesStore.editAssigneeIndex,
+  (newIndex) => {
+    if (newIndex !== null && assigneesStore.assignees[newIndex]) {
+      const assignee = assigneesStore.assignees[newIndex];
+      firstname.value = assignee.firstname;
+      lastname.value = assignee.lastname;
+      email.value = assignee.email;
+      phone.value = assignee.phone;
+    } else {
+      resetForm();
+    }
+  },
+  { immediate: true }
+);
+
+// Methods
+function resetForm() {
+  if (form.value) form.value.resetValidation();
+  firstname.value = '';
+  lastname.value = '';
+  email.value = '';
+  phone.value = '';
+  valid.value = false;
+  assigneesStore.clearEditAssigneeIndex();
+}
+
 function handleCancel() {
+  resetForm();
   router.push('/assignees');
 }
 
 function submit() {
+
+  const submission = {
+    firstname: firstname.value,
+    lastname: lastname.value,
+    email: email.value,
+    phone: phone.value,
+    important: false
+  };
+
+  if (assigneesStore.editAssigneeIndex !== null && assigneesStore.assignees[assigneesStore.editAssigneeIndex]) {
+    // Update existing assignee
+    assigneesStore.updateAssignee(assigneesStore.editAssigneeIndex, submission);
+  } else {
+    // Add new assignee
+    assigneesStore.addAssignee(submission);
+  }
+
+  resetForm();
   router.push('/assignees');
 }
 </script>
